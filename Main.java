c74d9bdb64054f1d312b48620ac50de418a6300a
hello1234512345 <schulmm4@my.erau.edu>
2025-10-20 16:06:35 -0400

    Update and rename Main_Pickle.java to Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main_Pickle {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MenuHandler_Pickle menuHandler = new MenuHandler_Pickle(sc);
